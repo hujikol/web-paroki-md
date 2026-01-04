891a8cad67ce98ec442d2eda487c030f0d463e68
hujikol <nikolasnanda@gmail.com>
2026-01-04 23:56:28 +0700

    fix/adjust blog header dropdown

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -175,7 +175,7 @@ export default function Header() {
                                                 {item.title}
                                             </NavigationMenuTrigger>
                                             <NavigationMenuContent>
-                                                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                                                <ul className="grid w-max min-w-[100px] gap-3 p-4 md:block">
                                                     {item.items.map((subItem) => (
                                                         <ListItem
                                                             key={subItem.title}
